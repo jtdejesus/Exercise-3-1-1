d933159e24b10574fd4bd3a9258dbeecb790e083
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-10-21 20:05:28 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+    String monthNine = "September";
+    double monthNineDays =30;
+    System.out.println(monthNine + " had " + monthNineDays + " days ");
+
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
